0145e9217721a19945d1f3971c327a51cd66ca4a
Joe-qijunhe <qihe9584@uni.sydney.edu.au>
2022-07-16 14:25:39 +1000

    first commit

---
+++ TestAccuracy/AccuracyRecords/accuracy_test.py
@@ -0,0 +1,52 @@
+import json
+from statistics import mean, stdev
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __repr__(self) -> str:
+        return "({},{})".format(self.x, self.y)
+
+def load_json_data(file):
+    data = ""
+    with open(file, "r") as f:
+        data = f.read()
+    return json.loads(data)
+
+"""
+    Parse accuracy_records.json and print out the result
+"""
+def print_accuracy_result():
+    json_array = load_json_data("accuracy_records.json")
+    # key: position, value: list of errors
+    position_error_map = dict()
+    for json_obj in json_array:
+        x = json_obj.get("ref_x")
+        y = json_obj.get("ref_y")
+        error = json_obj.get("error")
+        pos = Position(x, y)
+        if position_error_map.get(pos) is None:
+            position_error_map[pos] = [error]
+        else:
+            position_error_map[pos].append(error)
+    # list to store all the errors
+    all_error = []
+    # traverse the map and calculate the mean and standard deviation for each position
+    for pos in position_error_map.keys():
+        error_list = position_error_map[pos]
+        all_error += error_list
+        avg_error = mean(error_list)
+        std_error = stdev(error_list)
+        print("{} mean error: {:.2f} m, stdev: {:.2f} m".format(pos, avg_error, std_error))
+    print("Overall, the mean error is {:.2f} m and stdev is {:.2f} m".format(mean(all_error), stdev(all_error)))
+
+if __name__ == "__main__":
+    print_accuracy_result()
